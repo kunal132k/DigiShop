abedcd1f47a0500a5e769b5ae4743d8ee0bdcab9
kunal132k <kunal132k@gmail.com>
2021-03-12 17:58:14 +0530

    update profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -34,9 +34,11 @@ const Header = () => {
                                     <LinkContainer to='/profile'>
                                         <NavDropdown.Item>Profile</NavDropdown.Item>
                                     </LinkContainer>
+                                    <LinkContainer to="/">
                                     <NavDropdown.Item onClick={logoutHandler} >
                                         Logout
                                     </NavDropdown.Item>
+                                    </LinkContainer>
                                 </NavDropdown>
                             ) : <LinkContainer to='/login' >
                                     <Nav.Link>
