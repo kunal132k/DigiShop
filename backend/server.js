73c71fea2686d943b1fef15512e83c5bb60b1c7c
kunal132k <kunal132k@gmail.com>
2021-03-25 04:18:04 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ if(process.env.NODE_ENV === 'developement') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -34,6 +31,17 @@ app.use('/api/upload', uploadRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
