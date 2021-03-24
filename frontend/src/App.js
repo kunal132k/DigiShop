c676f3380bde0863c8cf1280bb0c9d4d7b60558b
kunal132k <kunal132k@gmail.com>
2021-03-25 03:35:01 +0530

    search box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,7 @@ const App = () => {
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/' component={HomeScreen} exact />
+          <Route path='/search/:keyword' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
