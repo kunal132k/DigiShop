ecddd5958ffc461c3f5583852d41cb8d93fe770e
kunal132k <kunal132k@gmail.com>
2021-03-24 20:59:09 +0530

    Admin Update Product

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id
@@ -23,8 +24,18 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector((state) => state.productDetails)
   const { loading, error, product } = productDetails
 
-  useEffect(() => {
+  const productUpdate = useSelector((state) => state.productUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate
 
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      history.push('/admin/productlist')
+    } else {
       if (!product.name || product._id !== productId) {
         dispatch(listProductDetails(productId))
       } else {
@@ -33,14 +44,26 @@ const ProductEditScreen = ({ match, history }) => {
         setImage(product.image)
         setBrand(product.brand)
         setCategory(product.category)
-        setCountInStock(product.countInStock)
-        setDescription(product.description)    
+        setCountInStock(product.countInSock)
+        setDescription(product.description)
       }
-  }, [dispatch, history, productId, product])
+    }
+  }, [dispatch, history, productId, product, successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    //update product
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        description,
+        countInStock,
+      })
+    )
   }
 
   return (
@@ -50,6 +73,8 @@ const ProductEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
@@ -77,23 +102,23 @@ const ProductEditScreen = ({ match, history }) => {
             </Form.Group>
 
             <Form.Group controlId='image'>
-                <Form.Label>Image</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter image URL'
-                    value={image}
-                    onChange={(e) => setImage(e.target.value)}
-                ></Form.Control>
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter image url'
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
             </Form.Group>
 
             <Form.Group controlId='brand'>
-                <Form.Label>Brand</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter brand'
-                    value={brand}
-                    onChange={(e) => setBrand(e.target.value)}
-                ></Form.Control>
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter brand'
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
             </Form.Group>
 
             <Form.Group controlId='countInStock'>
@@ -107,23 +132,23 @@ const ProductEditScreen = ({ match, history }) => {
             </Form.Group>
 
             <Form.Group controlId='category'>
-                <Form.Label>Category</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter cCtegory'
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                ></Form.Control>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter category'
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
             </Form.Group>
 
             <Form.Group controlId='description'>
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='Enter description'
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                ></Form.Control>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
             </Form.Group>
 
             <Button type='submit' variant='primary'>
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -95,7 +95,7 @@ const ProductListScreen = ({ history, match }) => {
                                     {product.brand}
                                 </td>
                                 <td>
-                                    <LinkContainer to={`/product/${product._id}/edit`}>
+                                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
                                         <Button variant='light' className='btn-sm'>
                                             <i className='fas fa-edit'></i>
                                         </Button>
